46186fb67bb5e7c46f9df61a4ac0a5c3f950f029
Dylan <dylan.acevedo2843@gmail.com>
2024-04-16 18:21:39 -0500

    terminando la pagina

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
+import React from 'react';
+
 import { createRoot } from "react-dom/client";
-import React, { StrictMode } from "react";
+import { StrictMode } from "react";
 import Header from "./header";
 import Main from "./main";
 import Aside from "./aside";
 import Footer from "./footer";
+
 const root = createRoot(document.getElementById("root"));
+
 root.render(
     <StrictMode>
-        <Header></Header> 
-        <div className="container">
-            <div className="row">
-                <div className="col-md-8 py-4">
-                    <Main></Main>
+        <div className="container-fluid bg-dark p-0">
+        <Header /> 
+        <div className="container-fluid ">
+            <div className="row align-items-center">
+                <div className="col-md-8 py-4 d-flex">
+                    <Main />
                 </div>
                 <div className="col-md-4">
-                    <Aside></Aside>
+                    <Aside />
                 </div>
             </div>
         </div>
-        <Footer></Footer>            
+        <Footer />
+        </div>
+              
     </StrictMode>
 );
